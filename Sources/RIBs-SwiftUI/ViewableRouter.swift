21583224098dc7896d0b5712b1e71f8d7f50d96f
Flatout73 <fantom2259@gmail.com>
2021-07-19 12:40:47 +0300

    Bring back public for view

--- Sources/RIBs-SwiftUI/ViewableRouter.swift
+++ Sources/RIBs-SwiftUI/ViewableRouter.swift
@@ -24,7 +24,7 @@ import SwiftUI
 /// forming a tree of routers that drives the tree of view controllers. Router drives the lifecycle of its owned
 /// interactor. `Router`s should always use helper builders to instantiate children `Router`s.
 open class ViewableRouter<InteractorType, ViewControllerType: ViewControllable, Content: View>: Router<InteractorType> {
-    open internal(set) var view: Content
+    public var view: Content
 
     /// The corresponding `ViewController` owned by this `Router`.
     public let viewController: ViewControllerType
